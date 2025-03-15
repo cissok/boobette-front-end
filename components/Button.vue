c5b118476c7b477df50d69b13bde15b3ec9629bd
Junior Cissokho Papin <junior.cissokhopapin@lolelife.com>
2025-03-15 11:32:12 +0100

    Add cursor pointer Button

--- components/Button.vue
+++ components/Button.vue
@@ -5,7 +5,7 @@
 </template>
 <style scoped>
 .button{
-  @apply font-medium py-2 px-4 rounded transition-colors duration-300;
+  @apply font-medium py-2 px-4 rounded transition-colors duration-300 cursor-pointer;
   &.primary {
     @apply bg-yellow-300 text-gray-700 hover:bg-yellow-50 hover:text-gray-500; ;
   }
