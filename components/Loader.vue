cea5bc586cc62947f7a0b423179fa216a6c57f5c
Junior Cissokho Papin <junior.cissokhopapin@lolelife.com>
2025-03-21 03:30:13 +0100

    Add Loader component

---
+++ components/Loader.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="loader" />
+</template>
+
+<style scoped>
+.loader {
+  width: 25px;
+  height: 25px;
+  border: 4px solid #000000;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
